8c976c1b0e0f9490cee2035836e8675fb46fd3fc
Gabriel Balduíno <gabrielbalduino29@gmail.com>
2025-04-15 21:08:35 -0300

    Update

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia n1 = new Nokia("(31)92924922", "Nokia", "G10G123", 256);
+
+n1.Ligar();
+n1.ReceberLigacao();
+n1.InstalarAplicativo("Duolingo");
+
+Console.WriteLine("");
+
+Iphone i1 = new Iphone("(11)91524215", "Iphone 13", "APPLE13ADQ115", 512);
+
+i1.Ligar();
+i1.ReceberLigacao();
+i1.InstalarAplicativo("Discord");
